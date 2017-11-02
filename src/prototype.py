36b74bc9727a5a97828a3292b8882e58fc962cdc
James <jamespeterthornton@gmail.com>
2017-11-01 17:02:56 -0700

    Prototype is complete but not learning. Looking into U-Net

--- src/prototype.py
+++ src/prototype.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Reshape
-from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, GlobalAveragePooling3D, Input
+from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, ZeroPadding3D, GlobalAveragePooling3D, Input
 from keras.models import Model
 from keras import backend as K
 from keras.layers import Activation
@@ -19,7 +19,6 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.backend.tensorflow_backend import set_session
 from keras.callbacks import ModelCheckpoint
 
-from custom_datagenerator import DataGenerator
 import numpy as np
 import scipy
 import pandas as pd
@@ -27,18 +26,15 @@ import cv2
 import sys
 import os
 
-TEST_FILEPATH = "stage1_a3d/0a27d19c6ec397661b09f7d5998e0b14.a3d"
-
-SUBJECT_LABELS_FILE = "labels/stage1_labels.csv"
-
+TEST_FILE_PATH = "stage1_a3d/0a27d19c6ec397661b09f7d5998e0b14.a3d"
+SUBJECT_LABELS_PATH = "labels/stage1_labels.csv"
 INPUT_SHAPE = (256, 256, 132, 1)
-
-#DataGenerator()
+SAE_PATH = "weights/best_sae.h5"
+CNN_PATH = "weights/best_cnn.h5"
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 set_session(tf.Session(config = config))
-
 
 def read_header(infile):
     """Read image header (first 512 bytes)
@@ -170,7 +166,7 @@ def read_data(infile):
 # Test that a file can be read correctly, print original values vs div by max
 #TODO - update bc I moved the /= max bit into read_data
 def test_read (should_print):
-    data = read_data(TEST_FILEPATH)
+    data = read_data(TEST_FILE_PATH)
     if (should_print):
         print (data.shape)
         print (data[0:5][0:5][0:5])
@@ -186,7 +182,7 @@ def test_read (should_print):
 # based on - https://arxiv.org/pdf/1706.04970.pdf
 
 
-def get_model():
+def get_sae():
     print (INPUT_SHAPE)
  
     model = Sequential()
@@ -211,9 +207,27 @@ def get_model():
     
     model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
     model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2)))
 
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+ 
+    model.add(UpSampling3D(size=(2,2,2)))
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+
+    model.add(UpSampling3D(size=(2,2,2)))
     model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
     model.add(Activation('relu'))
+
+
 
     #decoder
 
@@ -227,6 +241,8 @@ def get_model():
 #    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
     model.add(Conv3D(16, kernel_size=(3, 3, 3), padding='same'))
     model.add(Activation('relu'))
+
+    model.add(ZeroPadding3D((0,0,1)))
 
     model.add(UpSampling3D(size=(2,2,2)))
     model.add(Conv3D(8, kernel_size=(3, 3, 3), padding='same'))
@@ -246,71 +262,49 @@ def get_model():
              
     return model
 
-"""
-def get_model():
-    print (INPUT_SHAPE)
- 
-    model = Sequential()
+def get_cnn():
+    sae = load_model(SAE_PATH)
+#    sae = get_sae()
+    print (sae.summary())
+    print ("Wha?")
 
-    #encoder
-
-   
-
-    model.add(MaxPooling3D(pool_size=(2, 2, 1), input_shape=INPUT_SHAPE))
-    #add stride? 1, see paper
-#    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same',
-    model.add(Conv3D(4, kernel_size=(3, 3, 3), padding='same'))
-
-#,
-#                     input_shape=INPUT_SHAPE))
-    model.add(Activation('relu'))
-#    model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+    x = sae.layers[15].output
     
-#    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
-#    model.add(Conv3D(4, kernel_size=(3, 3, 3), padding='same'))
-#    model.add(Activation('relu'))
-    #model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-   
-#    model.add(Flatten())
-
-#    model.add(Dense(8))
-#    model.add(Activation('relu'))
+    x = Conv3D(64, kernel_size=(3, 3, 3), padding='same', name = 'cnn_conv3d_4')(x)
+    x = Activation('relu', name='cnn_activation_4')(x)
+    x = ZeroPadding3D((1,1,1))(x)
+    x = Conv3D(64, kernel_size=(3, 3, 3), padding='same', name = 'cnn_conv3d_5')(x)
+    x = Activation('relu', name='cnn_activation_5')(x)
+    x = MaxPooling3D(pool_size=(2, 2, 2), name='cnn_max_pooling3d_3')(x)
     
-    #decoder
+    x = ZeroPadding3D((1,1,1))(x)
+    x = Conv3D(128, kernel_size=(3, 3, 3), padding='same', name = 'cnn_conv3d_6')(x)
+    x = Activation('relu', name='cnn_activation_6')(x)
+    x = ZeroPadding3D((1,1,1))(x)
+    x = Conv3D(128, kernel_size=(3, 3, 3), padding='same', name = 'cnn_conv3d_7')(x)
+    x = Activation('relu', name='cnn_activation_7')(x)
+    x = MaxPooling3D(pool_size=(2, 2, 2), name='cnn_max_pooling3d_4')(x)
+  
+    x = Flatten()(x)
 
-#    model.add(Dense(86507520))
-#    model.add(Dense(2703360))#5406720))#10813440))#21626880))#43253760))
-#    model.add(Activation('relu'))
-#    model.add(Reshape((128,128,165,32)))
-#    model.add(Reshape((128,128,165, 1)))
+    x = Dense(128, activation='relu')(x)
  
-    #model.add(UpSampling3D(size=(2,2,2)))
-#    model.add(Conv3D(32, kernel_size=(3, 3, 3), padding='same'))
-#    model.add(Conv3D(4, kernel_size=(3, 3, 3), padding='same'))
-#    model.add(Activation('relu'))
+    x = Dense(5, activation = 'sigmoid')(x)
+ 
+    model = Model(inputs = sae.input, outputs = x)
 
-#    model.add(UpSampling3D(size=(2,2,2)))
-    model.add(Conv3D(4, kernel_size=(3, 3, 3), padding='same'))
-    model.add(Activation('relu'))
+    for layer in sae.layers:
+        layer.trainable = False
 
-    model.add(Conv3D(1, kernel_size=(3, 3, 3), padding='same'))
-    model.add(Activation('linear'))
-
-    model.add(UpSampling3D(size=(2,2,1)))
-
+    model.compile(loss=keras.losses.categorical_crossentropy,
+            optimizer= keras.optimizers.SGD(lr=0.05))
     
     print (model.summary())
-    #sys.exit(0) #
-
-    model.compile(loss=keras.losses.mean_squared_error,
-            optimizer= keras.optimizers.SGD())
-             
     return model
 
-"""
 
 def get_subject_labels():
-    infile = SUBJECT_LABELS_FILE
+    infile = SUBJECT_LABELS_PATH
     df = pd.read_csv(infile)
     # Separate the zone and subject id into a df
     df['Subject'], df['Zone'] = df['Id'].str.split('_',1).str
@@ -324,8 +318,25 @@ def generator(sae):
     dir_path='stage1_a3d'
     image_names = os.listdir(dir_path)
     labels = get_subject_labels()
-    np.random.shuffle(image_names)
-    batch_size = 1
+    if sae is False:
+        image_names = list(set(labels["Subject"].values.tolist()))
+        """        image_names = [
+            'NO - 00360f79fd6e02781457eda48f85da90',
+            'NO - 0043db5e8c819bffc15261b1f1ac5e42',
+            'NO - 0050492f92e22eed3474ae3a6fc907fa',
+            '006ec59fa59dd80a64c85347eef810c7',
+            '0097503ee9fa0606559c56458b281a08']
+        """
+        for i in range(0, len(image_names)):
+            image_names[i] = image_names[i] + ".a3d"
+
+#    np.random.shuffle(image_names)
+#    image_names = image_names[:10]
+    batch_size = 10
+
+    zone_map = {
+        1: 2, 2: 2, 3:3, 4:3, 11:4, 13:4, 15:4, 12:5, 14:5, 16:5,
+        17:1, 7:1, 6:1, 10:1, 9:1, 8:1, 5:1}
 
     while True:
         for start in range(0, len(image_names), batch_size):
@@ -335,7 +346,8 @@ def generator(sae):
             ids_train_batch = image_names[start:end]
             for filename in ids_train_batch:
                 if sae is False:
-                    y = np.zeros((17))
+#                    y = np.zeros((17))
+                    y = np.zeros((5))
                     threat_list = labels.loc[labels['Subject'] == filename.split(".")[0]]
                     threat_iter = threat_list.iterrows()
                     while True:
@@ -346,7 +358,8 @@ def generator(sae):
                         if threat['Probability'] is 1:
                             zone = threat['Zone']
                             zone = int(zone[4:])
-                            y[zone-1] = 1
+                            mapped_zone = zone_map[zone]
+                            y[mapped_zone-1] = 1
                     y_batch.append(y)                            
                 filedata = read_data(dir_path + '/{}'.format(filename))
 
@@ -360,6 +373,7 @@ def generator(sae):
                 yield x_batch, x_batch
             else: 
                 y_batch = np.array(y_batch)
+                print (y_batch)
                 yield x_batch, y_batch
 
 
@@ -379,17 +393,32 @@ def load_data():
 
 #x = load_data()
 
-sae_checkpoint = ModelCheckpoint("weights/best_sae.h5", monitor='loss', verbose=1, save_best_only=True, mode='min')
-vgg_checkpoint = ModelCheckpoint("weights/best_sae.h5", monitor='loss', verbose=1, save_best_only=True, mode='min')
+sae_checkpoint = ModelCheckpoint(SAE_PATH, monitor='loss', verbose=1, save_best_only=True, mode='min')
+cnn_checkpoint = ModelCheckpoint(CNN_PATH, monitor='loss', verbose=1, save_best_only=True, mode='min')
 
-#log = get_model().fit_generator(generator=generator(), steps_per_epoch = 200, epochs = 1000, verbose=2, callbacks=[saecheckpoint])
+#log = get_sae().fit_generator(generator=generator(True), steps_per_epoch = 200, epochs = 1000, verbose=2, callbacks=[sae_checkpoint])
 
+
+
+cnn = load_model(CNN_PATH)
+cnn.compile(loss=keras.losses.categorical_crossentropy,
+        optimizer= keras.optimizers.SGD(lr=0.01))
+
+#cnn = get_cnn()
 gen = generator(False)
 x, y = next(gen)
+#y = [[1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1]]
 
-print (x)
-print (y)
-print ("derp...")
+log = cnn.fit(x=x, y=y, batch_size=5, epochs=10000, verbose=2, callbacks=[cnn_checkpoint])
+
+
+predictions = cnn.predict(x)
+print(predictions)
+
+
+#print(cnn.summary())
+
+#log = cnn.fit_generator(generator=generator(False), steps_per_epoch = 1, epochs = 1000, verbose = 2, callbacks=[cnn_checkpoint])
 
 """log = model.fit_generator(generator=train_generator(),
                     steps_per_epoch=np.ceil(float(len(ids_train_split)) / float(batch_size)),
